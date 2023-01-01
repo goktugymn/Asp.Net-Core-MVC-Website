2d964d556298dab2d03d49bd915223cf72c4be39
Göktuğ YAMAN <120640452+goktugymn@users.noreply.github.com>
2023-01-01 17:03:19 +0300

    Update Category.cs

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,14 @@
-﻿namespace Article.Models
+using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+namespace Article.Models
 {
     public class Category
     {
         public int CategoryID { get; set; }
+        
+        [Required(ErrorMessage="Kategori boş bırakılamaz")]
+        [StringLength(20,ErrorMessage = "Lütfen 3-20 karakter sınırını aşmayınız",MinimumLength =3)]
         public string CategoryName { get; set; }
         public string CategoryDescription { get; set; }
         public List<Book> Books { get; set; }
