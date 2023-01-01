06bc3416cf7b8190d1c7b7a049f0d085d452dcc0
Göktuğ YAMAN <goktugyaman22@gmail.com>
2023-01-01 17:22:13 +0300

    Create LoginController.cs

---
+++ ARTICLE/ARTICLE/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ARTICLE.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace ARTICLE.Controllers
+{
+    public class LoginController : Controller
+    {
+        Context c = new Context();
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(UserAdmin p)
+        {
+
+           
+            var adminuserinfo = c.UserAdmins.FirstOrDefault
+                (x => x.UserName == p.UserName && x.Password == p.Password);
+            if (adminuserinfo != null)
+            {
+                return RedirectToAction("Index", "UserAdmin");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
