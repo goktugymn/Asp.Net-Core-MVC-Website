1aa1e5d3a72e2a2a26119b72fb55979b5dd0eb52
Göktuğ YAMAN <goktugyaman22@gmail.com>
2023-01-01 17:33:08 +0300

    Delete ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Article.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
