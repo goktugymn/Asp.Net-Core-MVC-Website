b0d4165af5554be4347ed32aabd8950d5719b97b
Göktuğ YAMAN <goktugyaman22@gmail.com>
2023-01-01 17:22:39 +0300

    Create UserAdmin.cs

---
+++ ARTICLE/ARTICLE/Models/UserAdmin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ARTICLE.Models
+{
+	public class UserAdmin
+	{
+		[Key]
+		public int AdminID { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public string AdminRole { get; set; }
+	}
+}
