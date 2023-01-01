d6f3ec08d2457465ccbb18e97e9214bff7bff00a
Göktuğ YAMAN <goktugyaman22@gmail.com>
2023-01-01 17:22:34 +0300

    Create Context.cs

---
+++ ARTICLE/ARTICLE/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using static ARTICLE.Models.Context;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ARTICLE.Models
+{
+    
+        public class Context : DbContext
+        {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=DbCoreBook; integrated security=True;");
+
+            }
+            public DbSet<Book> Books { get; set; }
+            public DbSet<Category> Categories { get; set; }
+        }
+    }
+
