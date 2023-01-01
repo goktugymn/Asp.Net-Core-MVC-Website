041e2cfe39a72eb124383f7eeeb4309a1a7132d6
Göktuğ YAMAN <goktugyaman22@gmail.com>
2023-01-01 17:22:32 +0300

    Create Category.cs

---
+++ ARTICLE/ARTICLE/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Article.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
