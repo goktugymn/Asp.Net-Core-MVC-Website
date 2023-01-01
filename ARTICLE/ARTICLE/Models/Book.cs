6c11a5e268c13210df47766108a39f5efd79ad39
Göktuğ YAMAN <goktugyaman22@gmail.com>
2023-01-01 17:22:31 +0300

    Create Book.cs

---
+++ ARTICLE/ARTICLE/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace ARTICLE.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+        public string DecriptionofBook { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public int Stock { get; set; }
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
