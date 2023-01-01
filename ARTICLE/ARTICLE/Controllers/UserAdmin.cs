880d346ddaa7c174286a0ba574c8cf271f20dd0c
Göktuğ YAMAN <goktugyaman22@gmail.com>
2023-01-01 17:22:16 +0300

    Create UserAdmin.cs

---
+++ ARTICLE/ARTICLE/Controllers/UserAdmin.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ARTICLE.Controllers
+{
+	public class UserAdmin : Controller
+	{
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
